35c2aee4b9b2d156bfe728417a0279fd0fe5af6f
Neofytos Dimitriou <neo@jerily.cy>
2023-09-10 13:06:28 -0400

    pass cmake_current_binary_dir to tests

--- tests/all.tcl
+++ tests/all.tcl
@@ -1,7 +1,14 @@
 package require tcltest
-
 namespace import -force ::tcltest::test
 
-::tcltest::configure {*}$argv -singleproc true -testdir [file dirname [info script]]
+if { [llength $argv] == 0 } {
+    puts stderr "Usage: $argv0 libdir"
+    exit 1
+}
+
+lappend auto_path [lindex $argv 0]
+set argv [lrange $argv 1 end]
+
+::tcltest::configure -singleproc true -testdir [file dirname [info script]]
 
 exit [::tcltest::runAllTests]
